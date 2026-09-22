eaaee2c49bd581a65eff32b9644a9e7ab7e2983d
Tight Code <tightcode@poweryourprocess.ai>
2026-09-21 20:42:13 -0400

    tightcode 0.2.1

--- Formula/tightcode.rb
+++ Formula/tightcode.rb
@@ -5,15 +5,15 @@
 class Tightcode < Formula
   desc "AI code review on every git push"
   homepage "https://tightcode.dev"
-  version "0.2.0"
+  version "0.2.1"
   license :cannot_represent
 
   if Hardware::CPU.arm?
-    url "https://github.com/tightcode-dev/homebrew-tap/releases/download/v0.2.0/tightcode-0.2.0-darwin-arm64.tar.gz"
-    sha256 "55d0d60470425540791ac5e7b014b592a4efaf027d99c666b4eb3320ba67d92c"
+    url "https://github.com/tightcode-dev/homebrew-tap/releases/download/v0.2.1/tightcode-0.2.1-darwin-arm64.tar.gz"
+    sha256 "56ae0423c32944c00d6c2b572a86e37d61076d240cec5e7fa01876311cbcc0bd"
   else
-    url "https://github.com/tightcode-dev/homebrew-tap/releases/download/v0.2.0/tightcode-0.2.0-darwin-x64.tar.gz"
-    sha256 "d5409c6870a0d557a7da668352c7216ecb044f80c79b82b5ce7074d31208ea24"
+    url "https://github.com/tightcode-dev/homebrew-tap/releases/download/v0.2.1/tightcode-0.2.1-darwin-x64.tar.gz"
+    sha256 "5b178f348c40eb877fb69783875f5a9dbe72de300de7e848556a271add6ea930"
   end
 
   depends_on :macos
